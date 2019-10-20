bab02feb7d1424486c7312213d62af13568120ba
Nikhil Anand <nikhilanand1006@gmail.com>
2019-10-20 22:43:03 +0530

    Small changes

--- src/test/java/DriverCode.java
+++ src/test/java/DriverCode.java
@@ -1,7 +1,7 @@
 /*
  * This code runs various test cases.
  * Not related to the core components of the project
- */
+*/
 
 import IDEAAlgorithm.IDEA_Parallel.Parallel_IDEA;
 import IDEAAlgorithm.IDEA_Serial.Serial_IDEA;
@@ -11,13 +11,14 @@ import java.io.InputStreamReader;
 
 public class DriverCode {
 
-	@SuppressWarnings({"Duplicates"})
 	public static void main(String[] args) {
 		try {
 
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Enter your text : ");
-			String test = reader.readLine();
+			String test = "";
+//			System.out.print("Enter your text : ");
+//			String test = reader.readLine();
+//			System.out.println("Your Text : " + test);
 
 			String[] returnData, returnData1;
 			Parallel_IDEA parallel = new Parallel_IDEA();
@@ -25,26 +26,28 @@ public class DriverCode {
 			long encrypt, decrypt;
 
 //			String test = new DataClass().getData();
-			returnData = serial.doIDEAEncryption(test);
-//			returnData = parallel.doEncryptionParallel(test);
-			encrypt = Long.parseLong(returnData[1]);
-			returnData1 = serial.doIDEADecryption(returnData[0]);
-			System.out.println("Cipher Text : " + returnData[0]);
-//			returnData1 = parallel.doDecryptionParallel(returnData[0]);
-			decrypt = Long.parseLong(returnData1[1]);
-			System.out.println("Decrypted Text : " + returnData1[0]);
+//			returnData = serial.doIDEAEncryption(test);
+			returnData = parallel.doEncryptionParallel(test);
+//			encrypt = Long.parseLong(returnData[1]);
+//			System.out.println("Cipher Text : " + returnData[0]);
+//			returnData1 = serial.doIDEADecryption(returnData[0]);
+			returnData1 = parallel.doDecryptionParallel(returnData[0]);
+//			decrypt = Long.parseLong(returnData1[1]);
+//			System.out.println("Decrypted Text : " + returnData1[0]);
 //			Encryption Decryption Total
 //			System.out.println(encrypt + " " + decrypt + " " + (encrypt + decrypt));
 
-//			System.out.println(" =============== ");
+			test = new DataClass().getData();
 //			returnData = serial.doIDEAEncryption(test);
-////			returnData = parallel.doEncryptionParallel(test);
-//			encrypt = Long.parseLong(returnData[1]);
+			returnData = parallel.doEncryptionParallel(test);
+			encrypt = Long.parseLong(returnData[1]);
+			System.out.println("Cipher Text : " + returnData[0]);
 //			returnData1 = serial.doIDEADecryption(returnData[0]);
-////			returnData1 = parallel.doDecryptionParallel(returnData[0]);
-//			decrypt = Long.parseLong(returnData1[1]);
-//			System.out.println(encrypt + " " + decrypt + " " + (encrypt + decrypt));
-//			System.exit(4);
+			returnData1 = parallel.doDecryptionParallel(returnData[0]);
+			decrypt = Long.parseLong(returnData1[1]);
+			System.out.println("Decrypted Text : " + returnData1[0]);
+//			Encryption Decryption Total
+			System.out.println(encrypt + " " + decrypt + " " + (encrypt + decrypt));
 
 		} catch (Exception e) {
 			e.printStackTrace();
